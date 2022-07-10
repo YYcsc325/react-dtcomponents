311f2e6275e0703ed6645fcb5ccb73500a762749
yycsc <m13625744973@163.com>
2022-07-10 17:15:04 +0800

    版本

--- typings.d.ts
+++ typings.d.ts
@@ -1,2 +1,13 @@
 declare module '*.css';
 declare module '*.less';
+
+/** 获取对象value类型（转成联合类型） */
+declare type ValueOf<T> = T[keyof T];
+
+/** 获取对象key类型（转成联合类型） */
+declare type TransforObjectToArray<T = any> = keyof T;
+
+/** 获取函数参数类型 */
+declare type GetFucParamsType<T> = T extends (arg: infer P) => void
+  ? P
+  : string;
